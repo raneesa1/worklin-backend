9d2b6778a3feb3405b6a190b96373ceb4ea93c42
raneesa <raneesak73@gmail.com>
2024-10-03 18:21:32 +0530

    made changes in notification service

--- notification-service/src/presentation/server.ts
+++ notification-service/src/presentation/server.ts
@@ -9,30 +9,30 @@ admin.initializeApp({
 });
 
 // Example route to send a notification
-app.post("/send-notification", async (req, res) => {
-  const { token, title, body } = req.body;
+// app.post("/send-notification", async (req, res) => {
+//   const { token, title, body } = req.body;
 
-  try {
-    const message = {
-      notification: {
-        title,
-        body,
-      },
-      token: token,
-    };
+//   try {
+//     const message = {
+//       notification: {
+//         title,
+//         body,
+//       },
+//       token: token,
+//     };
 
-    const response = await admin.messaging().send(message);
-    console.log("Successfully sent message:", response);
-    res
-      .status(200)
-      .json({ success: true, message: "Notification sent successfully" });
-  } catch (error) {
-    console.log("Error sending message:", error);
-    res
-      .status(500)
-      .json({ success: false, message: "Failed to send notification" });
-  }
-});
+//     const response = await admin.messaging().send(message);
+//     console.log("Successfully sent message:", response);
+//     res
+//       .status(200)
+//       .json({ success: true, message: "Notification sent successfully" });
+//   } catch (error) {
+//     console.log("Error sending message:", error);
+//     res
+//       .status(500)
+//       .json({ success: false, message: "Failed to send notification" });
+//   }
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
