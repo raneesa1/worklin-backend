80fd65f260b7f80cfda310a0225f95ba1fc6720c
raneesa <raneesak73@gmail.com>
2024-10-05 09:14:27 +0530

    fix:fixed the error connecting db in notification service

--- notification-service/src/presentation/server.ts
+++ notification-service/src/presentation/server.ts
@@ -1,40 +1,46 @@
-const express = require("express");
-const admin = require("firebase-admin");
-const serviceAccount = require("./path/to/your-firebase-adminsdk.json");
+import express, { Application, NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express();
+dotenv.config();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+const app: Application = express();
+const PORT: number = Number(process.env.PORT) || 3002;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: "http://localhost:4200",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// app.use("/", skillRoutes(dependencies));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  const errorResponse = {
+    errors: [{ message: err?.message || "Something went wrong" }],
+  };
+
+  return res.status(500).json(errorResponse);
 });
 
-// Example route to send a notification
-// app.post("/send-notification", async (req, res) => {
-//   const { token, title, body } = req.body;
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`notification service running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
 
-//   try {
-//     const message = {
-//       notification: {
-//         title,
-//         body,
-//       },
-//       token: token,
-//     };
+startServer();
 
-//     const response = await admin.messaging().send(message);
-//     console.log("Successfully sent message:", response);
-//     res
-//       .status(200)
-//       .json({ success: true, message: "Notification sent successfully" });
-//   } catch (error) {
-//     console.log("Error sending message:", error);
-//     res
-//       .status(500)
-//       .json({ success: false, message: "Failed to send notification" });
-//   }
-// });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
