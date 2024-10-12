410eb1cdbc211ef253a3fedfb34a007d2f7300ff
raneesa <raneesak73@gmail.com>
2024-10-12 22:50:29 +0530

    fix:payment error

--- payment-service/src/presentation/server.ts
+++ payment-service/src/presentation/server.ts
@@ -28,18 +28,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  if (req.originalUrl === "payment/webhook") {
-    next();
+  if (req.originalUrl === "/payment/webhook") {
+    express.raw({ type: "application/json" })(req, res, next);
   } else {
     express.json()(req, res, next);
   }
 });
 
-app.post(
-  "payment/webhook",
-  express.raw({ type: "application/json" }),
-  paymentWebhookController(dependencies)
-);
+app.post("/payment/webhook", paymentWebhookController(dependencies));
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "access.log"),
