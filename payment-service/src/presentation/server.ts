0f3d3b3863e68fe551cc93907584def0f7be6a2b
raneesa <raneesak73@gmail.com>
2024-10-12 23:02:08 +0530

    added console in server

--- payment-service/src/presentation/server.ts
+++ payment-service/src/presentation/server.ts
@@ -28,6 +28,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
+  console.log(req.originalUrl,'conosling the original url ')
   if (req.originalUrl === "/payment/webhook") {
     express.raw({ type: "application/json" })(req, res, next);
   } else {
